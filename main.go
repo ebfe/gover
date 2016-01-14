81b51b94ebe02b58788d486c1be12e9b1fb31075
Michael Gehring <mg@ebfe.org>
2016-01-14 14:24:00 +0100

    support PE files

--- main.go
+++ main.go
@@ -3,60 +3,121 @@ package main
 import (
 	"debug/dwarf"
 	"debug/elf"
+	"debug/pe"
 	"encoding/binary"
 	"fmt"
 	"os"
 )
+
+type Binary interface {
+	DWARF() (*dwarf.Data, error)
+	Close() error
+
+	ReadAtVaddr(b []byte, vaddr uint64) (int, error)
+	PtrSize() uint
+}
+
+func openBinary(name string) (Binary, error) {
+	e, err := elf.Open(name)
+	if err == nil {
+		return &elfBinary{File: e}, nil
+	}
+	p, err := pe.Open(name)
+	if err == nil {
+		return &peBinary{File: p}, nil
+	}
+	return nil, err
+}
+
+type elfBinary struct {
+	*elf.File
+}
+
+func (e *elfBinary) ReadAtVaddr(b []byte, vaddr uint64) (int, error) {
+	for _, s := range e.Sections {
+		if vaddr >= s.Addr && vaddr < s.Addr+s.Size {
+			return s.ReadAt(b, int64(vaddr-s.Addr))
+		}
+	}
+	return 0, fmt.Errorf("addr not mapped")
+}
+
+func (e *elfBinary) PtrSize() uint {
+	switch e.Class {
+	case elf.ELFCLASS32:
+		return 4
+	case elf.ELFCLASS64:
+		return 8
+	default:
+		panic("unknown elf class")
+	}
+}
+
+type peBinary struct {
+	*pe.File
+}
+
+func (p *peBinary) ReadAtVaddr(b []byte, vaddr uint64) (int, error) {
+	base := p.imageBase()
+	for _, s := range p.Sections {
+		start := base + uint64(s.VirtualAddress)
+		end := start + uint64(s.Size)
+		if vaddr >= start && vaddr < end {
+			return s.ReadAt(b, int64(vaddr-(base+uint64(s.VirtualAddress))))
+		}
+	}
+	return 0, fmt.Errorf("addr not mapped")
+}
+
+func (p *peBinary) PtrSize() uint {
+	// FIXME?
+	switch p.OptionalHeader.(type) {
+	case *pe.OptionalHeader32:
+		return 4
+	case *pe.OptionalHeader64:
+		return 8
+	}
+	panic("unknown pe format")
+}
+
+func (p *peBinary) imageBase() uint64 {
+	switch oh := p.OptionalHeader.(type) {
+	case *pe.OptionalHeader32:
+		return uint64(oh.ImageBase)
+	case *pe.OptionalHeader64:
+		return oh.ImageBase
+	}
+	panic("unknown pe format")
+}
 
 type variable struct {
 	Addr uint64
 	Type dwarf.Type
 }
 
-func findSection(e *elf.File, addr uint64) *elf.Section {
-	for _, sect := range e.Sections {
-		if addr >= sect.Addr && addr < sect.Addr+sect.Size {
-			return sect
-		}
-	}
-	return nil
-}
-
-func readBytes(e *elf.File, vaddr, len uint64) ([]byte, error) {
-	s := findSection(e, vaddr)
-	if s == nil {
-		return nil, fmt.Errorf("no section for addr")
-	}
-	val := make([]byte, len)
-	_, err := s.ReadAt(val, int64(vaddr-s.Addr))
-	if err != nil {
-		return nil, err
-	}
-	return val, nil
-}
-
-func readString(e *elf.File, v *variable) (string, error) {
+func readString(b Binary, v *variable) (string, error) {
 	if v.Type.String() != "struct string" {
 		return "", fmt.Errorf("wrong type %q", v.Type.String())
 	}
 
-	val, err := readBytes(e, v.Addr, uint64(v.Type.Size()))
-	if err != nil {
+	val := make([]byte, v.Type.Size())
+	if _, err := b.ReadAtVaddr(val, v.Addr); err != nil {
 		return "", err
 	}
+
 	sptr := uint64(0)
 	slen := uint64(0)
-	switch e.Class {
-	case elf.ELFCLASS32:
+	switch b.PtrSize() {
+	case 4:
 		sptr = uint64(binary.LittleEndian.Uint32(val))
 		slen = uint64(binary.LittleEndian.Uint32(val[4:]))
-	case elf.ELFCLASS64:
+	case 8:
 		sptr = binary.LittleEndian.Uint64(val)
 		slen = binary.LittleEndian.Uint64(val[8:])
 	}
 
-	val, err = readBytes(e, sptr, slen)
-	if err != nil {
+	val = make([]byte, slen)
+	if _, err := b.ReadAtVaddr(val, sptr); err != nil {
 		return "", err
 	}
 
@@ -111,7 +172,7 @@ func findVariable(d *dwarf.Data, name string) (*variable, error) {
 }
 
 func findVersion(file string) (string, error) {
-	e, err := elf.Open(file)
+	e, err := openBinary(file)
 	if err != nil {
 		return "", err
 	}
