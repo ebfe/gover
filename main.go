3f09160c30afb37497f79f71cf4aabb85d47bb56
Michael Gehring <mg@ebfe.org>
2016-01-14 08:39:28 +0100

    switch to dwarf for variable lookup

--- main.go
+++ main.go
@@ -1,58 +1,109 @@
 package main
 
 import (
+	"debug/dwarf"
 	"debug/elf"
 	"encoding/binary"
 	"fmt"
 	"os"
 )
 
-func findSymbol(e *elf.File, symbol string) (*elf.Symbol, error) {
-	syms, err := e.Symbols()
-	if err != nil {
-		return nil, err
-	}
-
-	for _, sym := range syms {
-		if sym.Name == symbol {
-			return &sym, nil
-		}
-	}
-
-	return nil, nil
+type variable struct {
+	Addr uint64
+	Type dwarf.Type
 }
 
-func readString(e *elf.File, sym *elf.Symbol) (string, error) {
-	buf := make([]byte, sym.Size)
-	sect := e.Sections[sym.Section]
-	if _, err := sect.ReadAt(buf, int64(sym.Value-sect.Addr)); err != nil {
-		return "", err
-	}
-
-	straddr := uint64(0)
-	strlen  := uint64(0)
-	switch e.Class {
-	case elf.ELFCLASS32:
-		straddr = uint64(binary.LittleEndian.Uint32(buf))
-		strlen = uint64(binary.LittleEndian.Uint32(buf[4:]))
-	case elf.ELFCLASS64:
-		straddr = binary.LittleEndian.Uint64(buf)
-		strlen = binary.LittleEndian.Uint64(buf[8:])
-	}
-
-	sindex := -1 
-	for i, s := range e.Sections {
-		if s.Addr <= straddr && straddr < s.Addr + s.Size {
-			sindex = i
+func findSection(e *elf.File, addr uint64) *elf.Section {
+	for _, sect := range e.Sections {
+		if addr >= sect.Addr && addr < sect.Addr+sect.Size {
+			return sect
 		}
 	}
-	sect = e.Sections[sindex]
-	value := make([]byte, strlen)
-	if _, err := sect.ReadAt(value, int64(straddr-sect.Addr)); err != nil {
+	return nil
+}
+
+func readString(e *elf.File, v *variable) (string, error) {
+	if v.Type.String() != "struct string" {
+		return "", fmt.Errorf("wrong type %q", v.Type.String())
+	}
+	s := findSection(e, v.Addr)
+	if s == nil {
+		return "", fmt.Errorf("no section for addr")
+	}
+	val := make([]byte, v.Type.Size())
+	_, err := s.ReadAt(val, int64(v.Addr-s.Addr))
+	if err != nil {
 		return "", err
 	}
 
-	return string(value), nil
+	sptr := uint64(0)
+	slen := uint64(0)
+	switch e.Class {
+	case elf.ELFCLASS32:
+		sptr = uint64(binary.LittleEndian.Uint32(val))
+		slen = uint64(binary.LittleEndian.Uint32(val[4:]))
+	case elf.ELFCLASS64:
+		sptr = binary.LittleEndian.Uint64(val)
+		slen = binary.LittleEndian.Uint64(val[8:])
+	}
+
+	s = findSection(e, sptr)
+	if s == nil {
+		return "", fmt.Errorf("no section for addr")
+	}
+	val = make([]byte, slen)
+	_, err = s.ReadAt(val, int64(sptr-s.Addr))
+	if err != nil {
+		return "", err
+	}
+	return string(val), nil
+}
+
+func findVariable(d *dwarf.Data, name string) (*variable, error) {
+	dr := d.Reader()
+	for {
+		e, err := dr.Next()
+		if e == nil || err != nil {
+			return nil, err
+		}
+
+		if e.Tag != dwarf.TagVariable {
+			continue
+		}
+
+		aname, ok := e.Val(dwarf.AttrName).(string)
+		if !ok || aname != name {
+			continue
+		}
+		loc, ok := e.Val(dwarf.AttrLocation).([]uint8)
+		if !ok {
+			continue
+		}
+		if loc[0] != 3 {
+			return nil, fmt.Errorf("can't determine variable addr")
+		}
+		addr := uint64(0)
+		switch len(loc) {
+		case 5:
+			addr = uint64(binary.LittleEndian.Uint32(loc[1:]))
+		case 9:
+			addr = uint64(binary.LittleEndian.Uint64(loc[1:]))
+		default:
+			return nil, fmt.Errorf("unknown addr size")
+		}
+
+		off, ok := e.Val(dwarf.AttrType).(dwarf.Offset)
+		if !ok {
+			continue
+		}
+		typ, err := d.Type(off)
+		if err != nil {
+			return nil, err
+		}
+
+		return &variable{Addr: addr, Type: typ}, nil
+	}
+	return nil, nil
 }
 
 func findVersion(file string) (string, error) {
@@ -62,15 +113,18 @@ func findVersion(file string) (string, error) {
 	}
 	defer e.Close()
 
-	sym, err := findSymbol(e, "runtime.buildVersion")
+	d, err := e.DWARF()
 	if err != nil {
 		return "", err
 	}
-	if sym == nil {
+	v, err := findVariable(d, "runtime.buildVersion")
+	if err != nil {
+		return "", err
+	}
+	if v == nil {
 		return "", fmt.Errorf("can't find version symbol")
 	}
-
-	return readString(e, sym)
+	return readString(e, v)
 }
 
 func main() {
